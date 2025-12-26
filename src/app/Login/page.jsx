bfeeeeb5ff579a50f7765a244f2d5b92abfab8db
Istheaksayem <istheakahmed9345@gmail.com>
2025-12-26 16:48:25 +0600

    added my next js project

---
+++ src/app/Login/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/component/Title';
+import React from 'react';
+
+const login = () => {
+    return (
+        <div>
+            <Title>welcome to login page</Title>
+        </div>
+    );
+};
+
+export default login;
