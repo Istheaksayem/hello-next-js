5bb362d4c90c3a29f0ee419d25fd2d3a4182bdeb
Istheaksayem <istheakahmed9345@gmail.com>
2025-12-27 22:18:37 +0600

    added a profile and setting route

---
+++ src/app/dashboard/my-profile/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/component/Title';
+import React from 'react';
+
+const MyProfile = () => {
+    return (
+        <div>
+            <Title>Welcome to MyProfile</Title>
+        </div>
+    );
+};
+
+export default MyProfile;
---
+++ src/app/dashboard/settings/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/component/Title';
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <div>
+            <Title>settings</Title>
+        </div>
+    );
+};
+
+export default Settings;
